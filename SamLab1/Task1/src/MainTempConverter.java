4f91b24e88b4bd3f859d54bf37b2340f07b56630
Alexis <altaras0b@gmail.com>
2019-02-16 23:26:33 +0300

    add Try/Catch block

--- SamLab1/Task1/src/MainTempConverter.java
+++ SamLab1/Task1/src/MainTempConverter.java
@@ -9,14 +9,18 @@ public class MainTempConverter {
      * @param args the command line arguments.Arguments aren't passed
      */
     public static void main(String[] args) {
+       try {
+           System.out.println("Введите температуру в градусах Фаренгейта(°F):");
+           Scanner in = new Scanner(System.in);
 
-        System.out.println("Введите температуру в градусах Фаренгейта(°F):");
-        Scanner in = new Scanner(System.in);
-        double farenheitDegrees = in.nextDouble();
+           double farenheitDegrees = in.nextDouble();
 
-        TemperatureConverter temperature = new TemperatureConverter(farenheitDegrees);
-        System.out.printf("Температура в градусах Цельсия равна: %.2f %s%n ",temperature.getCelsiusTemperature(),"°C");
-
+           TemperatureConverter temperature = new TemperatureConverter(farenheitDegrees);
+           System.out.printf("Температура в градусах Цельсия равна: %.2f %s%n ", temperature.getCelsiusTemperature(), "°C");
+       }
+       catch (Exception e){
+           System.out.println("Вы ввели некорректное значение!");
+       }
 
     }
 }
