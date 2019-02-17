e13e87170cded753e08e6452fea221925ea1e998
Alexis <altaras0b@gmail.com>
2019-02-17 17:23:32 +0300

    Task5 completed! All code in Main cause program is very small

---
+++ SamLab1/Task5/src/MainNumberInput.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * Program which counts amount of numbers that user entered, counts sum of it and average
+ */
+public class MainNumberInput {
+    /**
+     * Entry point of program
+     * @param args the command line arguments.Arguments aren't passed
+     */
+    public static void main(String[] args) {
+        try {
+            System.out.println("Введите число с клавиатуры. \nКак только вы введёте 0 будет произведён рассчёт");
+            Scanner in = new Scanner(System.in);
+            double sum = 0;
+            int count = 0; // int count = -1; if don't need to include 0 to average and count
+            double number;
+            do {
+                number = in.nextDouble();
+                sum += number;
+                count++;
+            } while (number != 0);
+            System.out.printf("%s %d %s %.2f %s %.2f", "Было введено чисел:", count, "\nИх сумма равна:", sum,
+                    "\nИх среднее арифметическое равно:", sum / count);
+        }
+        catch (Exception e){
+            System.out.println("Вы ввели некорректное значение!");
+        }
+    }
+}
